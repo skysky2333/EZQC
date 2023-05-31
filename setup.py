8c5b084ddc57d097e8c1cbed5a4119b47b6c1aa8
Sky Li <lyxsky@hotmail.com>
2023-05-31 15:59:07 -0700

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ setup(
     name='ezqc',
     version='0.1',
     author='Tinger Shi & Sky Li',
-    long_description = 'EZQC is a streamlined, terminal-based alternative to FastQC. Instead of generating individual report files per analysis, EZQC displays the analysis results, reasons, and suggestions directly in the terminal, making it easier to quickly assess the quality of multiple files.\n\nAdditionally, EZQC generates figures for each analysis, providing a visual aid to spot potential issues for further examination.',
+    long_description='''
+    EZQC
+    ====
+
+    EZQC is a streamlined, terminal-based alternative to FastQC. Instead of generating individual report files per analysis, EZQC displays the analysis results, reasons, and suggestions directly in the terminal, making it easier to quickly assess the quality of multiple files.
+
+    Additionally, EZQC generates figures for each analysis, providing a visual aid to spot potential issues for further examination.
+    ''',
     packages=find_packages(),
     install_requires=[
         # Add your project dependencies here
